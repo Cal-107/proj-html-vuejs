e4096ae67f288bd9887dbfee87834b5a5e6dc72e
“Cal-107” <kaly___7@hotmail.it>
2021-12-10 12:04:11 +0100

    add slider and buttons components

---
+++ src/components/PrevNextBtn.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="btn-prev-next prev-tr">
+      <span class="text-1">prev</span>
+    </div>
+
+    <div class="btn-prev-next next-tr">
+      <span class="text-2">next</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PrevNextBtn",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/variables";
+@import "@/styles/utilities";
+</style>
---
+++ src/components/Slider1.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="slider-container">
+    <div class="img-box d-flx justContCent alignIteCent">
+      <img class="urban-bg" src="../assets/img/h3-rev-img-5.png" alt="" />
+      <img class="img-absolute" src="../assets/img/h3-rev-img-6.png" alt="" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Slider1",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/variables";
+@import "@/styles/utilities";
+
+.slider-container {
+  position: relative;
+  padding-top: 1rem;
+  padding-bottom: 4rem;
+  .img-box {
+    padding: 1rem 0;
+    .urban-bg {
+      width: 65%;
+    }
+    .img-absolute {
+      width: 25%;
+      position: absolute;
+      top: 53%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
+  }
+}
+</style>
